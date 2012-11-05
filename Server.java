3e8f456dc5619b41e4ba9cdbe1a2d44a5087cd29
Alex <aaralis@purdue.edu>
2012-11-04 22:27:45 -0500

    I just changed so many things...
    I don't even want to talk about it but...
    added the enum requirment
    still no assertions... ill get that in a bit
    now to run Server it must be passed the port it will be using in args[]
    as in
    java Server 8188(or some other port numer that's just mine)
    to quit the server you still just type exit into the console
    not totally commented yet... ill get it later
    added some suporting methods
    changed a few variable names

--- Server.java
+++ Server.java
@@ -6,14 +6,14 @@ class Server{
   public TCPChannel channel;//probably should have a get method for the request and responder channels but made them public insead because im lazy
   private LinkedList<Integer> requestersID;//list of backlogged requesters
   private LinkedList<Integer> respondersID;//list of backlogged responders
-  private LinkedList<String> requestersLocation;//list of places that the requesters are located at.  Corresponds to the requester ID list
+  private LinkedList<Location> requestersLocation;//list of places that the requesters are located at.  Corresponds to the requester ID list
   private LinkedList<String> respondersTeam;//list of teams that the responders are on.  Corresponds to the responder ID list.
   
   public Server(int port){//init of all the object variables for the server
     channel = new TCPChannel(port);
     requestersID = new LinkedList<Integer>();
     respondersID = new LinkedList<Integer>();
-    requestersLocation = new LinkedList<String>();
+    requestersLocation = new LinkedList<Location>();
     respondersTeam = new LinkedList<String>();
     
     channel.setMessageListener(new MessageListener(){
@@ -21,39 +21,80 @@ class Server{
         
         try{
           if(message.charAt(7) == ':'){//if message is from the requester then...
-            String currentRequestersLocation = message.substring(7, message.length()); //removes the starting Request: from the recieved message string
+            Location location = encodeLocationString(message.substring(7, message.length()));
+            
+            if(location == null){
+              System.out.println("Requester message failure!!!");
+              channel.sendMessage("The location you submitted was not recognized as a valid location!!!", clientID);
+              return;
+            }
             
             if(respondersID.size() == 0){//if there are no responders on call then...
               channel.sendMessage("Searching:", clientID); //tells the requester that it needs to wait
               requestersID.add(clientID); //adds the requester ID to the backlog
-              requestersLocation.add(currentRequestersLocation);//adds the requester location to the backlog
+              requestersLocation.add(location);//adds the requester location to the backlog
               
-            }else{//if there are no responders then...
+            }else{//if there are responders then...
               channel.sendMessage("Assigned:" + respondersTeam.removeFirst(), clientID);//sends a message to the requester telling them a responder has been assigned and what team that responder is on
-              channel.sendMessage("Assigned:" + currentRequestersLocation, respondersID.removeFirst());//tells the longest waiting responder they have been assigned and the location of the requester they are to help
+              channel.sendMessage("Assigned:" + locationToString(location), respondersID.removeFirst());//tells the longest waiting responder they have been assigned and the location of the requester they are to help
             }
             
           }else if(message.charAt(8) == ':'){//if the message is from responders
-            String currentRespondersTeam = message.substring(8, message.length());//removes the stating Response: form the recieved message string
+            String teamString = message.substring(8, message.length());
             
             if(requestersID.size() == 0){//if there are no backlogged requesters then...
               channel.sendMessage("Searching:", clientID);//tell the responder they need to wait
               respondersID.add(clientID);//log the responder ID
-              respondersTeam.add(currentRespondersTeam);//log the responders team
+              respondersTeam.add(teamString);//log the responders team
               
             }else{//if there are backlogged requesters then...
-              channel.sendMessage("Assigned:" + currentRespondersTeam, requestersID.removeFirst());//tell the longest waiting requester a responder has been assigned and the team of that responder
-              channel.sendMessage("Assigned:" + requestersLocation.removeFirst(), clientID);//tell the responder they have been assigned and the location of the requester they are picking up
+              channel.sendMessage("Assigned:" + teamString, requestersID.removeFirst());//tell the longest waiting requester a responder has been assigned and the team of that responder
+              channel.sendMessage("Assigned:" + locationToString(requestersLocation.removeFirst()), clientID);//tell the responder they have been assigned and the location of the requester they are picking up
             }
-          }else {
-            System.out.println("message format error!!!");
+          } else {
+            System.out.println("message format error. ':' in the wrong place!!!");
+            channel.sendMessage("message format error. ':' in the wrong place!!!", clientID);
           }
         }catch(ChannelException e){//catch for all that channel stuff
-          System.out.println("message sending failure!!!");
+          System.out.println("Exception occured while sending message!!!");
           e.printStackTrace();
         }
       }
     });
+  }
+  
+  private Location encodeLocationString(String locationString) throws ChannelException{
+    
+    if (locationString.equals("CL50 - Class of 1950 Lecture Hall")){
+      return Location.CL50;
+    } else if (locationString.equals("EE - Electrical Engineering Building")){
+      return Location.EE;
+    } else if (locationString.equals("LWSN - Lawson Computer Science Building")){
+      return Location.LWSN;
+    } else if (locationString.equals("PMU - Purdue Memorial Union")){
+      return Location.PMU;
+    } else if (locationString.equals("PUSH - Purdue University Student Health Center")){
+      return Location.PUSH;
+    } else {
+      return null;
+    }
+  }
+  
+  private String locationToString(Location loc){
+    switch (loc) {
+      case CL50:
+        return "CL50 - Class of 1950 Lecture Hall";
+      case EE:
+        return "EE - Electrical Engineering Building";
+      case LWSN:
+        return "LWSN - Lawson Computer Science Building";
+      case PMU:
+        return "PMU - Purdue Memorial Union";
+      case PUSH:
+        return "PUSH - Purdue University Student Health Center";
+      default:
+        return null;
+    }
   }
   
   public static void main(String[] args){
@@ -73,4 +114,3 @@ class Server{
     }
   }
 }
-
